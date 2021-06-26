ae379e46c60342765fb92b909f56d6a8d5d4019d
Surisetti-Kesava-Satya-Kumar <86141262+Surisetti-Kesava-Satya-Kumar@users.noreply.github.com>
2021-06-26 19:08:50 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Info(){
+  return(
+    <div className="note">
+      <h1>Full Stack Web dev Bootcamp</h1>
+      <p>it is a Complete Web dev Bootcamp</p>
+      </div>
+
+  );
+}
+
+
+export default Info;
